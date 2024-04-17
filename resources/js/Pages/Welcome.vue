9a2ed241e8ce0d8f7489f05249386242e69c37a2
Alan <alanrodrigo.mg@gmail.com>
2024-04-17 13:13:17 -0400

    utilizando componentes como button primary y colocando a todas las secciones

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,6 +1,9 @@
 <script setup>
 import { Head, Link } from '@inertiajs/vue3';
+
 import JetApplicationMark from '@/Components/ApplicationMark.vue';
+import JetPrimaryButton from '@/Components/PrimaryButton.vue';
+
 import Section from '@/Personal-Components/Section.vue';
 
 defineProps({
@@ -36,14 +39,33 @@ defineProps({
     <Section class="bg-gray-800 pt-16 h-screen">
         <div class="h-2/3 flex flex-wrap content-between border-gray-600 pb-36">
             <p class="border-b-2 font-bold border-gray-400 pb-3 text-2xl text-gray-300 uppercase">Hey! This is Alan. I'm a Software Engineer and I would like to work with you.</p>
+            <div class="flex items-end border-b-2 border-gray-500 pb-2 ml-1">
+                <p class="font-bold mr-5 text-gray-500 text-xl">Want to know more?</p>
+                <jet-primary-button class="bg-green-400 rounded font-bold text-sm text-gray-800 hover:bg-green-500">
+                    Let's Chat
+                </jet-primary-button>
+            </div>
+        </div>
+        <div class="animate-pulse mb-3 text-gray-300 text-center text-9xl">
+            <a href="#skills">&#8675;</a>
         </div>
     </Section>
 
-    <Section class="bg-gray-600 text-gray-200 h-screen">
+    <Section id="skills" class="bg-gray-600 text-gray-200 h-screen">
         <h2 class="text-6xl font-bold">Skill</h2>
+        <div class="flex justify-center mt-10">
+            <jet-primary-button class="bg-indigo-800 rounded font-bold text-sm text-gray-200 hover:bg-indigo-700">
+                Get in touch
+            </jet-primary-button>
+        </div>
     </Section>
     <Section class="bg-gray-300 text-gray-800 h-screen">
         <h2 class="text-6xl font-bold">Projects</h2>
+        <div class="flex justify-center mt-10">
+            <jet-primary-button class="bg-purple-100 rounded font-bold text-sm text-gray-800 hover:bg-purple-200">
+                Know more
+            </jet-primary-button>
+        </div>
     </Section>
 
     <Section class="flex justify-between bg-gray-800 text-gray-300 test-xl">
