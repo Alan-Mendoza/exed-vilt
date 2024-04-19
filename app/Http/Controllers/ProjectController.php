c9dbe619261c03d080a507a9efd2bddb230e218e
Alan <alanrodrigo.mg@gmail.com>
2024-04-19 17:32:07 -0400

    creando la vista index projects

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Project;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class ProjectController extends Controller
 {
-    //
+    public function index()
+    {
+        return Inertia::render('Projects/index', [
+            'projects' => Project::all(),
+        ]);
+    }
 }
--- resources/js/Pages/Projects/index.vue
+++ resources/js/Pages/Projects/index.vue
@@ -2,8 +2,17 @@
     import AppLayout from '@/Layouts/AppLayout.vue';
     import JetPrimaryButton from '@/Components/PrimaryButton.vue';
 
+    import { defineAsyncComponent, reactive } from 'vue'; // Funcion Async Component para cargar los iconos de Heroicons
+    import * as HeroiconsSolid from '@heroicons/vue/24/solid'; // Importar todos los iconos de Heroicons
+
+    const myMethod = (projects, index) => {
+    const iconName = projects[index].icon_name;
+    const IconComponent = HeroiconsSolid[`${iconName}Icon`];
+    return defineAsyncComponent(() => Promise.resolve(IconComponent));
+};
+
     defineProps({
-        skills: Object,
+        projects: Object,
     });
 </script>
 
@@ -11,36 +20,48 @@
     <AppLayout title="Skills">
         <template #header>
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                Skills
+                Projects
             </h2>
         </template>
 
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <table v-if="skills.length > 0" class="w-full">
+                <table v-if="projects.length > 0" class="w-full">
                     <thead class="border-b-2 border-gray-300 text-indigo-600">
                         <tr>
                             <th class="px-6 py-3">
-                                Name
+                                Title
+                            </th>
+                            <th class="px-6 py-3">
+                                Description
                             </th>
                             <th class="px-6 py-3">
                                 Color
+                            </th>
+                            <th class="px-6 py-3">
+                                Icon
                             </th>
                             <th class="px-6 py-3">
                                 Actions
                             </th>
                         </tr>
                     </thead>
-                    <tbody v-for="skill in skills" :key="skill.id">
+                    <tbody v-for="(project, index) in projects" :key="project.id">
                         <tr class="text-sm text-indigo-900 border-b border-gray-400">
                             <td class="px-6 py-4">
-                                {{ skill.name }}
+                                {{ project.title }}
+                            </td>
+                            <td class="px-6 py-4">
+                                {{ project.description }}
                             </td>
                             <td class="px-6 py-4">
                                 <p class="flex items-center">
-                                    <div class="mr-5 p-4 rounded h-10 w-5" :class="skill.color"></div>
-                                    {{ skill.color }}
+                                    <div class="mr-5 p-4 rounded h-10 w-5" :class="project.color"></div>
+                                    {{ project.color }}
                                 </p>
+                            </td>
+                            <td class="px-6 py-4" :class="project.color">
+                                <component :is="myMethod(projects, index)"></component>
                             </td>
                             <td class="px-6 py-4 text-center">
                                 <jet-primary-button class="border border-indigo-500 text-indigo-500 bg-indigo-400 hover:bg-indigo-300 mr-2">
